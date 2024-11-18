8c2afa1d6d035f7013bd5066bed9e2f54b635e96
Chinin0 <cristhianarauz06@gmail.com>
2024-11-18 00:04:35 -0400

    cambios de chinin

--- src/App.js
+++ src/App.js
@@ -1,24 +1,44 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
+import { BrowserRouter as Router, Route, Routes, Link } from 'react-router-dom';
+import Home from './components/home';
+import CreateClient from './components/CreateClient';
+import './components/css/App.css';
 
 function App() {
+  const [message, setMessage] = useState('');
+
+  useEffect(() => {
+    axios.get('http://localhost:3000/api/hello/')
+      .then(response => {
+        setMessage(response.data.message);
+      })
+      .catch(error => {
+        console.error("Error fetching data: ", error);
+      });
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div className="app-container">
+        <header className="app-header">
+          <h1>Gestión de Clientes</h1>
+          <p className="api-message">{message}</p>
+        </header>
+
+        <nav className="app-nav">
+          <Link to="/" className="nav-link">Inicio</Link>
+          <Link to="/create-client" className="nav-link">Crear Cliente</Link>
+        </nav>
+
+        <main className="app-content">
+          <Routes>
+            <Route path="/" element={<Home message={message} />} />
+            <Route path="/create-client" element={<CreateClient />} />
+          </Routes>
+        </main>
+      </div>
+    </Router>
   );
 }
 
